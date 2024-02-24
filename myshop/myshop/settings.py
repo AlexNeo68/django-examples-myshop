4287b4ad357715c3c3c5343879643b008820d9da
Alexander <alexander@iMac-Alexander.local>
2024-02-24 10:13:24 +0300

    cart in start

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     
     # local
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
