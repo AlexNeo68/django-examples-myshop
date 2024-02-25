73e1d244ab4aa36f6b94a7fb3c8a1feb8acadcbc
Alexander <alexander@iMac-Alexander.local>
2024-02-25 10:15:27 +0300

    orderd

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+from .forms import OrderCreateForm
+from .models import OrderItem
+
+def create_order(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, 
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'],
+                                         )
+            cart.clear()
+
+            context = {
+                'order': order
+            }
+            return render(request, 'orders/order/created.html', context)
+    
+    else:
+        form = OrderCreateForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'orders/order/create.html', context)
